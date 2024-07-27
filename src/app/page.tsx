d15db777231fa7032b77cfed2bb41cb24f1a0adf
Moch. Fariz Al Hazmi <mochfarizalhazmi@gmail.com>
2024-07-27 08:48:57 +0700

    Slicing setup layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,13 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
+  return (
+    <main>
+      <Button >Submit</Button>
+    </main>
+  );
+  /*
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
@@ -110,4 +117,5 @@ export default function Home() {
       </div>
     </main>
   );
+  */
 }
